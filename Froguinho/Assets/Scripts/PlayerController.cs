320ab41f3a3b51b97d8ce2e41933d1587a91276a
Nyteon-Wol <MatheusEdDominguesGanancio@Gmail.com>
2025-11-16 02:08:09 -0300

    Run State

--- Froguinho/Assets/Scripts/PlayerController.cs
+++ Froguinho/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public Rigidbody2D theRB;
     public float jumpForce;
+    public float runSpeed;
+
+    private float activeSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,7 +21,20 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // não precisa usar "if" pq o valor é negativo apertando pra esquerda ou positivo apertando pra direita
-        theRB.linearVelocity = new Vector2( Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.linearVelocity.y);
+        // Vector2 usa os eixos X e Y, no caso está multiplicando o input da unity "Horizontal" pela variavel "modeSpeed"
+        // e aplicando na linearVelocity.y, assim faz se mover baseado no valor da variavel para velocidade
+        //theRB.linearVelocity = new Vector2( Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.linearVelocity.y);
+
+
+        // faz com que quando o player aperte o LeftShift, a activeSpeed se torne runSpeed, para alterar a velocidade
+        // de andando para correndo
+        activeSpeed = moveSpeed;
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            activeSpeed = runSpeed;
+        }
+
+        theRB.linearVelocity = new Vector2( Input.GetAxisRaw("Horizontal") * activeSpeed, theRB.linearVelocity.y);
 
         if(Input.GetButtonDown("Jump"))
         {
