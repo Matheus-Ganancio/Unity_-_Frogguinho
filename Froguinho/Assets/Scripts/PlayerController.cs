57be75762d7eb47518fb0bb4847965193b3354de
Nyteon-Wol <MatheusEdDominguesGanancio@Gmail.com>
2025-11-16 00:22:06 -0300

    Adicionado pulo

--- Froguinho/Assets/Scripts/PlayerController.cs
+++ Froguinho/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D theRB;
+    public float jumpForce;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,6 +17,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // não precisa usar "if" pq o valor é negativo apertando pra esquerda ou positivo apertando pra direita
         theRB.linearVelocity = new Vector2( Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.linearVelocity.y);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            theRB.linearVelocity = new Vector2(theRB.linearVelocity.x, jumpForce);
+        }
     }
 }
