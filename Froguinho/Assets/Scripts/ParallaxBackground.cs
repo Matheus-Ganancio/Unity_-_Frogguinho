efcc3ad66f368f41b373d7655a888954c308d9c0
Nyteon-Wol <MatheusEdDominguesGanancio@Gmail.com>
2025-12-14 17:36:36 -0300

    refinado o controle do parallax do cenário

--- Froguinho/Assets/Scripts/ParallaxBackground.cs
+++ Froguinho/Assets/Scripts/ParallaxBackground.cs
@@ -6,6 +6,9 @@ public class ParallaxBackground : MonoBehaviour
 
     public Transform sky;
     public Transform treeline;
+    [Range(0f, 1f)]
+
+    public float parallaxSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,5 +19,12 @@ public class ParallaxBackground : MonoBehaviour
     void LateUpdate()
     {
         sky.position = new Vector3(theCam.position.x, theCam.position.y, sky.position.z);
+
+        // controle para que o conteudo da variavel "treeline" se mova se baseando na posição X e Y da
+        // camera(variavel "theCam") pela velocidade setada na variavel "parallaxSpeed"
+        treeline.position = new Vector3(
+            theCam.position.x * parallaxSpeed,
+            theCam.position.y * parallaxSpeed,
+            treeline.position.z);
     }
 }
